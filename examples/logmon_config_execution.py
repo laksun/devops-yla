787456648cde770252d1f56fcd2bbca4489d20c3
Levent Aksun <ylaksun@yahoo.com>
2024-09-03 15:13:13 +0100

    test get_s3_export

--- examples/logmon_config_execution.py
+++ examples/logmon_config_execution.py
@@ -70,4 +70,21 @@ def replace_fluenta_placeholders( fluentd_config, file , region,
     except Exception:
         logging.warning("Unable to remove parameter file.")
 
+app_logging_bucket_export = "ApplicationLoggingBucket"        
+def get_s3_export(aws_region, use_proxy):
+    cf_client = get_service_client("cloudformation", aws_region, use_proxy)
+    try:
+        applogs_s3_bucket = {"Exports: []"}
+        applogs_s3_bucket = cf_client.exports()
+    except Exception as ex:
+        logging.error(f"Unable to get applogs S3 bucket: {ex}")
+        pass
+    exports = applogs_s3_bucket["Exports"] 
+    s3_export = None
+    for export in exports:
+        if export["Name"] == app_logging_bucket_export:
+            s3_export = export["Value"]
+    logging.info(f"Found S3 export: {s3_export}")
+    return s3_export           
+
 
